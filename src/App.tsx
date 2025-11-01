b3f326f4e77b5e32abe53627c6c5c5f791961043
danielaestefania19 <danielacastellano1000@gmail.com>
2025-11-01 00:41:21 -0600

    Create Configuration

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+       <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg shadow-lg hover:bg-blue-600">
+        ¡Tailwind funciona! 🎉
+      </button>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
