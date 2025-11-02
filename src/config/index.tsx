c93009737227657cfd6e8f682811202d2543131e
Alfredo Castellano <alfredocastellanoula.14@gmail.com>
2025-11-02 06:58:08 -0600

    Last Deploy

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -2,7 +2,8 @@ import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { mainnet, arbitrumSepolia, scrollSepolia } from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
 
-export const projectId = "1d089db2baa74b6f4d8ce33d7ea5571f"
+export const projectId = import.meta.env.VITE_PROJECT_ID
+if (!projectId) throw new Error("Falta el PROJECT ID de AppKit")
 
 console.log("PROJECT ID:", projectId)
 export const metadata = {
